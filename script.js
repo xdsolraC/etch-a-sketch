a17768ecc71427ec289622ebc5a4890bb54f3c6b
xdsolraC <xds0lr4c@gmail.com>
2024-02-14 23:39:56 -0400

    JS: add createGrid function and variables to be used

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ const clearBtn = document.querySelector(".clearBtn");
 const colorPicker = document.querySelector(".color-picker");
 const randomBtn = document.querySelector("#random");
 let sketchPx = 400;
+let gridSizeValue = parseInt(gridSizeElem.textContent);
+let colorValue = colorPicker.value;
+let randomBtnValue = randomBtn.checked;
+
+/* FUNCTIONS */
+function createGrid(gridSize) {
+    // Create squares function based on the gridSize:
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement("div");
+        const squareSize = sketchPx / gridSize;
+        square.classList.add("grid-square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        // Append a draw event listener to each square when created:
+        square.addEventListener("mouseover", () => {
+            if (randomBtnValue) {
+                square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+            } else {
+                square.style.backgroundColor = colorValue;
+            }
+        })
+
+        // Append each square to the sketch:
+        sketch.appendChild(square);
+    }
+}
